9c140708026c7e552daeaa4ba67ac692118b520a
Brian Kelley <Mac@Brians-MacBook-Pro.local>
2014-04-04 17:54:12 -0500

    completed most of concept

--- index.php
+++ index.php
@@ -39,10 +39,159 @@
 			<!-- End Home Slider -->
 		</section>	
 
-		<section class="home-tabs">
+		<section class="home-tabs tabs">
+			<div class="tabbed-nav--container wrapper">			
+				<section class="tabbed-nav--row">
+				
+					<a data-reveal="tab1" class="is-current tabbed-nav--item gd-quarter gt-quarter" href="#">
+						<div class="tabbed-nav--label">Steals & Deals</div>
+					</a>
+					<a data-reveal="tab2" class="tabbed-nav--item gd-quarter gt-quarter" href="#">
+						<div class="tabbed-nav--label">Free Consultation</div>
+					</a>
+					<a data-reveal="tab3" class="tabbed-nav--item gd-quarter gt-quarter" href="#">
+						<div class="tabbed-nav--label">Service & Repair</div>
+					</a>
+					<a data-reveal="tab4" class="tabbed-nav--item gd-quarter gt-quarter" href="#">
+						<div class="tabbed-nav--label">Safety Tips</div>
+					</a>						
+				</section>				
+			</div>
+	
+			
+			<!-- Tab 1 -->
+			<section id="tab1" class="tab is-current tabbed-content--container">
+				<div class="wrapper">
+					<div class="tabbed-content--body gd-row">
+						<div class="gd-half gd-columns">
+							<img class="prs" src="http://placehold.it/478x336">
+						</div>
+						<div class="gd-half gd-columns">
+							<h3 class="tabbed-content--heading great-text">Steals & Deals</h3>
+							<p class="primer-text thin mtxs">This series stands out for its most appropriate use for interior partitions and spaces where the boundary between interior and exterior are almost nonexistent along with acoustic and thermal insulation that doesn't have considerable impact.</p>
+							<a class="button primary small gm-full" href="#"><span class="button--text">Learn More</span></a>
+						</div>
 
+					</div>
+				</div>
+			</section>
+			<!-- End Tab 1 -->
 
+			<!-- Tab 2 -->
+			<section id="tab2" class="tab tabbed-content--container">
+				<div class="wrapper">
+					<div class="tabbed-content--body gd-row gt-row">
+						<div class="gd-half gd-columns gt-half gt-columns">
+							<img class="prs" src="http://placehold.it/300x250">
+						</div>
+						<div class="gd-half gd-columns gt-half gt-columns">
+							<h3 class="primary-heading semibold uppercase paragon-text">Steals & Deals</h3>
+							<p>A special point of pride for us is our garage door showroom.  There is no other garage door display quite like ours in Ventura and Santa Barbara Counties.  Featuring a wide selection of garage doors, you'll have an opportunity to see fully functional garage doors and garage door openers and customize your garage door features before buying.</p>
+							<a class="button special" href="minimal-frame/index.php">Learn More</a>
+						</div>
+
+					</div>
+				</div>
+			</section>
+			<!-- End Tab 2 -->
+
+			<!-- Tab 3 -->
+			<section id="tab3" class="tab tabbed-content--container">
+				<div class="wrapper">
+					<div class="tabbed-content--body gd-row">
+						<div class="gd-half gd-columns">
+							<img class="prs" src="images/windows-doors/custom-wood.jpg">
+						</div>
+						<div class="gd-half gd-columns">
+							<h3 class="primary-heading semibold uppercase paragon-text">Custom Wood</h3>
+							<p>This series is perfect for a historic restoration or the current day aesthetic touch. In our wood line, we use a state of the art finishing process that uses Nano Technology that allows the wood to breathe but prevents moisture from penetrating the wood.</p>
+							<a class="button special" href="custom-wood/index.php">Learn More</a>
+						</div>
+
+					</div>
+				</div>
+			</section>
+			<!-- End Tab 3 -->
+
+			<!-- Tab 4 -->
+			<section id="tab4" class="tab tabbed-content--container">
+				<div class="wrapper">
+					<div class="tabbed-content--body gd-row">
+						<div class="gd-half gd-columns">
+							<img class="prs" src="images/windows-doors/elite-aluminum.jpg">
+						</div>
+						<div class="gd-half gd-columns">
+							<h3 class="primary-heading semibold uppercase paragon-text">Elite Aluminum</h3>
+							<p>Perfect for a modern design or to maximize the visible glass. This series offers sleek and clean lines with smooth a operation every time. Through multiple features, this series will stand the test of time while making a design statement.</p>
+							<a class="button special" href="elite-aluminum/index.php">Learn More</a>
+						</div>
+
+					</div>
+				</div>
+			</section>
+			<!-- End Tab 4 -->			
+		<!--End Tabs-->
 		</section>
+
+		<section class="home-showcase">
+			<div class="wrapper">
+				<hr class="dashed-rule">
+				<div class="gd-row gt-row">
+					<div class="gd-half gd-columns gt-half gt-columns">
+						<section class="home-showcase--container">
+							<a class="home-showcase--overlay first">
+								<span class="home-showcase--heading petite-text">Garage Doors</span>
+								<img class="home-showcase--img" src="http://placehold.it/700x363">
+							</a>
+						</section>
+					</div>
+					<div class="gd-half gd-columns gt-half gt-columns">
+						<section class="home-showcase--container">
+							<a class="home-showcase--overlay">
+								<span class="home-showcase--heading petite-text">Entry Gates</span>
+								<img class="home-showcase--img" src="http://placehold.it/700x363">
+							</a>
+						</section>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="home-projects">
+			<div class="home-projects--top">
+				<div class="wrapper">
+					<h3 class="great-text tertiary-heading align-center mtn">Commercial Projects</h3>
+					<ul class="inline-bulleted-list paragon-text align-center">
+						<li>Doors</li>
+						<li>Dock Levelers</li>
+						<li>Access Control</li>
+						<li>24 Hr Response</li>
+					</ul>
+				</div>
+			</div>
+			<div class="home-projects--bottom">
+				<div class="wrapper">
+					<div class="gd-row gt-row">
+						<div class="gd-half gd-columns gt-half gt-columns">						
+								<h3 class="white-text canon-text bold capitalize">Why our customers love us.</h3>
+								<p class="block-quote paragon-text thin white-text">I was quite impressed with your employees. Both your sales rep and technician were knowlegable and respectful and made buying my new garage door so easy! Thanks!</p>
+								<p class="paragon-text">— Richard K, Simi Valley</p>					
+						</div>
+						<div class="gd-half gd-columns gt-half gt-columns">
+							<img src="http://placehold.it/560x500">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="home-visit">
+			<div class="wrapper">
+
+			</div>
+		</section>
+
+
 
 	</main>
 
